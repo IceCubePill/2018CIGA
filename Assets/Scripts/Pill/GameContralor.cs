c0c388c9643d00ae0f8f205b5f5538030781a3f9
Luyao Huang <lhuang325@gatech.edu>
2018-07-07 20:28:40 +0800

    Level 1

---
+++ Assets/Scripts/Pill/GameContralor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameContralor : MonoBehaviour
+{
+    public CharacterContral P1_Contral;
+    public CharacterContral P2_Contral;
+    public  Vector2 SavePosition_P1;
+    public Vector2 SavePosition_P2;
+    // Use this for initialization
+    void Start () {
+		Object.DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public  void QuickSave(Vector2 p1,Vector2 p2)
+    {
+        SavePosition_P1 = p1;
+        SavePosition_P2 = p2;
+    }
+    public void QuickLoad()
+    {
+        P1_Contral.transform.position = SavePosition_P1;
+        P2_Contral.transform.position = SavePosition_P2;
+
+    }
+
+}
---
+++ Assets/Scripts/Pill/SavePoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class SavePoint : MonoBehaviour
+{
+    private BoxCollider2D bc2d;
+	// Use this for initialization
+    private Vector2 SavePosition_P1=Vector2.zero;
+    private Vector2 SavePosition_P2=Vector2.zero;
+    private GameContralor contralor;
+	void Start ()
+	{
+	    bc2d = GetComponent<BoxCollider2D>();
+	    contralor = FindObjectOfType<GameContralor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D c2d)
+    {
+        if (c2d.gameObject.layer==9)
+        {
+            if (c2d.GetComponent<CharacterContral>().IsP2)
+            {
+                SavePosition_P2 = c2d.transform.position;
+            }
+            else
+            {
+                SavePosition_P1 = c2d.transform.position;
+            }
+        }
+
+        if (SavePosition_P1!=Vector2.zero && SavePosition_P2!=Vector2.zero)
+        {
+            contralor.QuickSave(SavePosition_P1,SavePosition_P2);
+        }
+    }
+}

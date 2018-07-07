6c1f3a95be4ede0b20811391c31fa78c768e1357
IceCubepill <icecubepill@163.com>
2018-07-07 11:45:31 +0800

    7/7 11.45
    被吸引时不能移动

--- Assets/Scripts/Pill/CharacterContral.cs
+++ Assets/Scripts/Pill/CharacterContral.cs
@@ -21,18 +21,24 @@ public class CharacterContral : MonoBehaviour
     private MagenetedObject mo;
 
     private Rigidbody2D r2d;
+    private CircleCollider2D circleCollider;
+    private Magenet mage;
     // Use this for initialization
     void Start ()
     {
         sr = GetComponent<SpriteRenderer>();  
         mo = GetComponent<MagenetedObject>();
         r2d = GetComponent<Rigidbody2D>();
+        mage = GetComponentInChildren<Magenet>();
+        circleCollider = GetComponent<CircleCollider2D>();
     }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
 	    CheckInput();
+	    CheckFloor();
+
 
 	}
     
@@ -40,15 +46,14 @@ public class CharacterContral : MonoBehaviour
     {
         if (!IsP2)
         {
-            t_right = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
-            if (t_right != 0)
+            if (!isflood)
             {
-                transform.Translate(t_right * Speed * Time.fixedDeltaTime, 0, 0);
-                
-
+                 t_right = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
+                if (t_right != 0)
+                {
+                     transform.Translate(t_right * Speed * Time.fixedDeltaTime, 0, 0);
+                }
             }
-
-
             if ((Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.K)) && !(Input.GetKey(KeyCode.J) && Input.GetKey(KeyCode.K)))
             {
                 if (Input.GetKey(KeyCode.J))
@@ -70,12 +75,16 @@ public class CharacterContral : MonoBehaviour
         }
         else
         {
-            t_right = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) - (Input.GetKey(KeyCode.LeftArrow) ? 1 : 0);
-            if (t_right != 0)
+            if (!isflood)
             {
-                transform.Translate(t_right * Speed * Time.fixedDeltaTime, 0, 0);
-            }
+                t_right = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) - (Input.GetKey(KeyCode.LeftArrow) ? 1 : 0);
+                if (t_right != 0)
+                {
+                 transform.Translate(t_right * Speed * Time.fixedDeltaTime, 0, 0);
+                }
 
+            }
+          
 
             if ((Input.GetKey(KeyCode.Keypad4) || Input.GetKey(KeyCode.Keypad6)) && !(Input.GetKey(KeyCode.Keypad4) && Input.GetKey(KeyCode.Keypad6)))
             {
@@ -95,6 +104,22 @@ public class CharacterContral : MonoBehaviour
                 sr.color = Color.white;
                 mo.maganet = MagenetedObject.Maganet.None;
             }
+        }
+    }
+
+    void CheckFloor()
+    {
+       RaycastHit2D rh2d= Physics2D.Raycast(transform.position, Vector2.down, circleCollider.radius + 0.5f, (1 << 12) );
+        if (rh2d==false)
+        {
+           Debug.Log("jump");
+           isflood = true;
+        }
+        else
+        {
+            Debug.Log("land");
+            isflood = false;
+         
         }
     }
 }

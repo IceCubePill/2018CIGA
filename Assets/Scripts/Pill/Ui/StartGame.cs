bb83fe034f4f44c5b390d9883bae74828c68765f
IceCubepill <icecubepill@163.com>
2018-07-08 14:24:04 +0800

    开始界面ok

---
+++ Assets/Scripts/Pill/Ui/StartGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public  GameObject panle;
+	// Use this for initialization
+	void Start () {
+		Object.DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.anyKey)
+	    {
+	        panle.SetActive(true);
+
+        }
+	}
+
+    public void LoadScene(int i)
+    {
+        StartCoroutine(Loading(i));
+
+    }
+
+    IEnumerator Loading(int i)
+    {
+        SceneManager.LoadScene(5);
+        panle.SetActive(false);
+       AsyncOperation ao= SceneManager.LoadSceneAsync(i);
+        ao.allowSceneActivation = false;
+        while (!ao.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        ao.allowSceneActivation = true;
+    }
+}
